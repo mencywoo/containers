7fb80f47bd15134b61ded68189eaddcdf5a3c09e
Mency Woo <mency.woo@gmail.com>
2016-10-28 15:26:40 -0700

    experiment so far

---
+++ flask-app/webapp/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, request
+from flaskext.mysql import MySQL
+app = Flask(__name__)
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config.from_envvar('CONFIG')
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'LindenHof11'
+# app.config['MYSQL_DATABASE_DB'] = 'menagerie'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route("/")
+def hello():
+    return "Welcome to Python Flask App!"
+
+@app.route("/pets")
+def Authenticate():
+    owner = request.args.get('Owner')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT name, species from pet where owner='" + owner + "'")
+    data = cursor.fetchall()
+    if str(data) == '()':
+        return "No pets for " + owner
+    else:
+        print "data", str(data)
+#    else:
+        a=""
+        for row in data:
+            a = a+", "+ str(row[0]) + "(" + str(row[1]) +  ")"
+        return a[1:]
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
